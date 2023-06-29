1340b49eaebd9ed959455be057b53e3e85b43516
ok-home <ok-home@ya.ru>
2023-06-29 23:37:13 +0700

    LL

--- main/main.c
+++ main/main.c
@@ -19,41 +19,23 @@ void logic_analyzer_ws_server(void);
 static const char *TAG = "example";
 
 #define RMT_TX_CHANNEL RMT_CHANNEL_0
+__attribute__((always_inline))
+static inline void rmt_ll_tx_stop(rmt_dev_t *dev, uint32_t channel)
+{
+    dev->conf_ch[channel].conf1.tx_start = 0;
+    dev->conf_ch[channel].conf1.mem_rd_rst = 1;
+    //dev->conf_ch[channel].conf1.mem_rd_rst = 0;
+    dev->conf_ch[channel].conf1.apb_mem_rst = 1;
+    //dev->conf_ch[channel].conf1.apb_mem_rst = 0;
+}
 
-/*
- * Prepare a raw table with a message in the Morse code
- *
- * The message is "ESP" : . ... .--.
- *
- * The table structure represents the RMT item structure:
- * {duration, level, duration, level}
- *
- */
-/*
-static const rmt_item32_t morse_esp[] = {
-    // E : dot
-    {{{ 32767, 1, 32767, 0 }}}, // dot
-    {{{ 32767, 0, 32767, 0 }}}, // SPACE
-    // S : dot, dot, dot
-    {{{ 32767, 1, 32767, 0 }}}, // dot
-    {{{ 32767, 1, 32767, 0 }}}, // dot
-    {{{ 32767, 1, 32767, 0 }}}, // dot
-    {{{ 32767, 0, 32767, 0 }}}, // SPACE
-    // P : dot, dash, dash, dot
-    {{{ 32767, 1, 32767, 0 }}}, // dot
-    {{{ 32767, 1, 32767, 1 }}},
-    {{{ 32767, 1, 32767, 0 }}}, // dash
-    {{{ 32767, 1, 32767, 1 }}},
-    {{{ 32767, 1, 32767, 0 }}}, // dash
-    {{{ 32767, 1, 32767, 0 }}}, // dot
-    // RMT end marker
-    {{{ 0, 1, 0, 0 }}}
-};*/
 #define R1M {{{ 5, 1, 5, 0 }}}
 #define R1F {{{ 10, 1, 10, 0 }}}
 #define R1EOF {{{ 0, 1, 0, 0 }}}
+#define TX_THRES sizeof(morse_esp) / sizeof(morse_esp[0])
 static const rmt_item32_t morse_esp[]=
 {
+/*
 R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,
 R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,
 R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,
@@ -68,40 +50,43 @@ R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,
 R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,
 R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,
 R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,
+*/
 R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,
 R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,R1M,
 R1F,R1EOF
 };
 
-static int cnt=0;
 #define RMT_LL_EVENT_TX_THRES(channel)    (1 << ((channel) + 24))
 #define RMT_LL_EVENT_TX_DONE(channel)     (1 << ((channel) * 3))
 
 void IRAM_ATTR fn_tx_isr(void *arg)
 {
-    uint32_t status = rmt_ll_tx_get_interrupt_status(&RMT, RMT_TX_CHANNEL)
+    static int cnt=0;
+    uint32_t status = rmt_ll_tx_get_interrupt_status(&RMT, RMT_TX_CHANNEL);
     if(status & RMT_LL_EVENT_TX_THRES(RMT_TX_CHANNEL) ){
     if(cnt++ > 10) {
-        //rmt_tx_stop(RMT_TX_CHANNEL);
+        rmt_ll_tx_enable_loop(&RMT,RMT_TX_CHANNEL, false);
+        rmt_set_tx_thr_intr_en(RMT_TX_CHANNEL,false,TX_THRES);
+
+        rmt_ll_clear_interrupt_status(&RMT, RMT_LL_EVENT_TX_DONE(RMT_TX_CHANNEL));
+        rmt_set_tx_intr_en(RMT_TX_CHANNEL,true);
         cnt=0;
-        rmt_set_tx_thr_intr_en(RMT_TX_CHANNEL,false,258);
-        rmt_set_tx_loop_mode(RMT_TX_CHANNEL, false);
         }
     rmt_ll_clear_interrupt_status(&RMT, RMT_LL_EVENT_TX_THRES(RMT_TX_CHANNEL));
     gpio_set_level(19,1);
     gpio_set_level(19,0);
-
-    }
+    //return;
+    } 
     if(status & RMT_LL_EVENT_TX_DONE(RMT_TX_CHANNEL) ){
-        rmt_tx_stop(RMT_TX_CHANNEL);
         rmt_set_tx_intr_en(RMT_TX_CHANNEL,false);
         rmt_ll_clear_interrupt_status(&RMT, RMT_LL_EVENT_TX_DONE(RMT_TX_CHANNEL));
+//        rmt_ll_tx_stop(&RMT,0);
+//rmt_tx_stop(0);
         gpio_set_level(19,1);
         gpio_set_level(19,0);
         gpio_set_level(19,1);
         gpio_set_level(19,0);
     }
-
 }
 /*
  * Initialize the RMT Tx channel
@@ -109,27 +94,13 @@ void IRAM_ATTR fn_tx_isr(void *arg)
 static void rmt_tx_init(void)
 {
     rmt_config_t config = RMT_DEFAULT_CONFIG_TX(CONFIG_EXAMPLE_RMT_TX_GPIO, RMT_TX_CHANNEL);
-    // enable the carrier to be able to hear the Morse sound
-    // if the RMT_TX_GPIO is connected to a speaker
-    //config.tx_config.carrier_en = false;
     config.tx_config.loop_en = true;
-
-    //config.tx_config.carrier_duty_percent = 50;
-    // set audible career frequency of 611 Hz
-    // actually 611 Hz is the minimum, that can be set
-    // with current implementation of the RMT API
-    //config.tx_config.carrier_freq_hz = 611;
-    // set the maximum clock divider to be able to output
-    // RMT pulses in range of about one hundred milliseconds
     config.clk_div = 8;
-    config.mem_block_num = 8;
+    config.mem_block_num = 5;
 
     ESP_ERROR_CHECK(rmt_config(&config));
-//    ESP_ERROR_CHECK(rmt_driver_install(config.channel, 0, 0));
 
     rmt_isr_register(fn_tx_isr,NULL,ESP_INTR_FLAG_EDGE | ESP_INTR_FLAG_IRAM | ESP_INTR_FLAG_LOWMED ,NULL);
-//    rmt_register_tx_end_callback(fn_tx_end,NULL);
-//    rmt_set_tx_thr_intr_en(RMT_TX_CHANNEL,true,258);
 }
 
 void app_main(void)
@@ -141,27 +112,24 @@ void app_main(void)
     gpio_reset_pin(26);
     gpio_set_direction(26,GPIO_MODE_OUTPUT);
 
-
     rmt_tx_init();
 
     rmt_fill_tx_items(RMT_TX_CHANNEL, morse_esp, sizeof(morse_esp) / sizeof(morse_esp[0]),0);
-    //rmt_tx_start(RMT_TX_CHANNEL, true);
 
     while (1) {
-    //    ESP_ERROR_CHECK(rmt_write_items(RMT_TX_CHANNEL, morse_esp, sizeof(morse_esp) / sizeof(morse_esp[0]), true));
-    //    ESP_LOGI(TAG, "Transmission complete");
-    //    rmt_set_tx_thr_intr_en(RMT_TX_CHANNEL,true,257);
     gpio_set_level(26,1);
     gpio_set_level(26,0);
 
-    rmt_fill_tx_items(RMT_TX_CHANNEL, morse_esp, sizeof(morse_esp) / sizeof(morse_esp[0]),0);
-    rmt_set_tx_thr_intr_en(RMT_TX_CHANNEL,true,258);
-    rmt_set_tx_intr_en(RMT_TX_CHANNEL,true);
-    rmt_set_tx_loop_mode(RMT_TX_CHANNEL, true);
-    rmt_tx_start(RMT_TX_CHANNEL, true);
+    //rmt_tx_init();
+    //rmt_fill_tx_items(RMT_TX_CHANNEL, morse_esp, sizeof(morse_esp) / sizeof(morse_esp[0]),0);
+
+//    rmt_ll_clear_interrupt_status(&RMT, RMT_LL_EVENT_TX_DONE(RMT_TX_CHANNEL));
+    rmt_ll_clear_interrupt_status(&RMT, RMT_LL_EVENT_TX_THRES(RMT_TX_CHANNEL));
+    rmt_set_tx_thr_intr_en(RMT_TX_CHANNEL,true,TX_THRES);
+//    rmt_set_tx_intr_en(RMT_TX_CHANNEL,false);
+
+            rmt_ll_tx_enable_loop(&RMT,RMT_TX_CHANNEL, true);
+    //rmt_tx_start(RMT_TX_CHANNEL, true);
         vTaskDelay(20 / portTICK_PERIOD_MS);
-//    rmt_set_tx_thr_intr_en(RMT_TX_CHANNEL,false,258);
-//    rmt_tx_stop(RMT_TX_CHANNEL);
-//        vTaskDelay(10 / portTICK_PERIOD_MS);
     }
 }
